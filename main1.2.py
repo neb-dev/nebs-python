c0322a9db0091eaae56082c2f6594d5b4265f690
neb <10528790@my.uvu.edu>
2021-04-05 22:32:56 -0600

    functions, conditionals, and/or, elif

---
+++ main1.2.py
@@ -0,0 +1,38 @@
+# Functions
+def mean(value):
+    # Conditionals
+    if type(value) == dict:
+        result = sum(value.values()) / len(value)
+    elif type(value) == list:
+        result = sum(value) / len(value)
+    else:
+        return print("invalid data type")
+        
+    return result
+
+print(type(sum), type(mean))
+
+my_list = [10, 100, 1000]
+print(mean(my_list))
+
+# result example sum(my_dictionary.values()) / len(my_dictionary)
+my_dictionary = {"Josh": 20, "Jessica": 200, "Bailey": 2000}
+print(mean(my_dictionary))
+
+my_tuple = (30, 300, 3000)
+print(mean(my_tuple))
+
+x = 1
+y = 1
+
+# AND
+if x == 1 and y == 1:
+    print("Yes AND")
+else:
+    print("No AND")
+
+# OR
+if x == 1 or y == 2:
+    print("Yes OR")
+else:
+    print("No OR")
