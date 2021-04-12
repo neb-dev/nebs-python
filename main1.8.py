df701d94d0252560ef6e84ddec146712359eb76b
neb-dev <10528790@my.uvu.edu>
2021-04-11 20:16:11 -0600

    keyword args, default/non-default args, sorted

---
+++ main1.8.py
@@ -0,0 +1,20 @@
+# Keyword Arguments
+# def concat_strings(a = "Hello", b) will throw an error due to invalid order of default/non-default arugment. a being default arugment b being non-default
+def concat_strings(a = "Hello", b = "World"): 
+    return a + " " + b
+
+print(concat_strings())
+print(concat_strings("Goodbye"))
+
+# Arbitary number of arugments
+def mean(*args): # args can be named whatever you'd like
+    return sum(args) / len(args)
+
+print(1,8,14,21)
+
+def sort_strings(*args):
+    strings = [arg.upper() for arg in args]
+    # strings.sort()
+    return sorted(strings)
+
+print(sort_strings("Hello", "Goodbye", "Tempalte", "Unique", "Zebra"))
