06283d2896b70ba4e9fff62073c7beb563a1e392
neb <10528790@my.uvu.edu>
2021-05-15 23:28:08 -0600

    with context manager

--- main1.9.py
+++ main1.9.py
@@ -1,9 +1,13 @@
 # create a file object
-txt_file = open("fruits.txt")
+# txt_file = open("fruits.txt")
 
-fruit = txt_file.read()
+# fruit = txt_file.read()
 
-txt_file.close()
+# txt_file.close()
+
+# with
+with open("fruits.txt") as txt_file:
+    fruit = txt_file.read()
 
 # display file contents
 print(fruit)
