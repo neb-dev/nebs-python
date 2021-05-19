6009fcaf04469e3d21dbb89d36346c56d3ca6423
neb <10528790@my.uvu.edu>
2021-05-18 22:28:25 -0600

    use os module to check if file path exists

--- main2.py
+++ main2.py
@@ -2,8 +2,20 @@
 # import sys
 # sys.builtin_module_names
 import time
+import os
 
 while True:
-    with open("files/vegetables.txt") as file:
-        print(file.read())
-        time.sleep(5)
+    if os.path.exists("files/vegetables.txt"):
+        with open("files/vegetables.txt") as file:
+            print(file.read())
+    else:
+        print("file not found")
+    time.sleep(5)
+
+# os modules
+# import os
+# sys.prefix
+# open /Library/Frameworks/Python.framework/Versions/3.9
+# dir > python3.9 > os.py
+# dir(os)
+# os.path.exists("files/vegetables.txt") = True
