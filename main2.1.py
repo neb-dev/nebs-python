5df621bf84aa2f5f5b81f10d0a8e9296829ebbc2
neb <10528790@my.uvu.edu>
2021-05-18 23:02:45 -0600

    using the panda lib

--- main2.1.py
+++ main2.1.py
@@ -7,9 +7,16 @@ import os
 import pandas
 
 while True:
-    with open("files/temps.txt") as file:
-        if os.path.exists("files/temps.txt"):
-            print(file.read())
+    with open("files/temps.csv") as file:
+        if os.path.exists("files/temps.csv"):
+            data = pandas.read_csv("files/temps.csv")
+            print(data.mean()) # print(data.mean()["st1"]) will print st1 column mean
         else:
             print("file not found")
         time.sleep(5)
+
+# import pandas
+# data = pandas.read_csv("files/temps.csv")
+# data = prints view of temps.csv content
+# type(data) = <class 'pandas.core.frame.DataFrame'>
+# DataFrame is a unique data type to the pandas lib
