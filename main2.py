0a2face8b0d4bb00573cb7ba3aeaf1b5d54c5a95
neb <10528790@my.uvu.edu>
2021-05-18 22:11:32 -0600

    import builtin time module

--- main2.py
+++ main2.py
@@ -1,1 +1,9 @@
-#
+# Built In Modules
+# import sys
+# sys.builtin_module_names
+import time
+
+while True:
+    with open("files/vegetables.txt") as file:
+        print(file.read())
+        time.sleep(5)
