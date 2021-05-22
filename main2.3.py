e76abfed1d50b0b1ae29a29c29b462faa06ddd1c
neb <10528790@my.uvu.edu>
2021-05-21 20:42:48 -0600

    using ipython and creating a pandas DataFrame

---
+++ main2.3.py
@@ -0,0 +1,9 @@
+# pip3.9 install ipython
+# ipython
+# import pandas
+# df1=pandas.DataFrame([[2,4,6],[10,20,30]],columns=["col1","col2","col3"],index=["row1","row2"])
+# df2=pandas.DataFrame([{"Name":"Josh","Initials":"JN"},{"Name":"Jessica","Initials":"JN"},{"Name":"Bailey","Initials":"BN"}])
+# dir(df1)
+# df1.mean() - Returns pandas.Series data type
+# df1.row1.mean()
+# df1.row1.max()
