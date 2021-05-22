78e11238f67afd0d62259f6e1231c80a5db73ca7
neb <10528790@my.uvu.edu>
2021-05-21 21:06:32 -0600

    installing and testing with jupyter library with pandas

---
+++ jupyter/main2.4.py
@@ -0,0 +1,6 @@
+# pip3.9 install jupyter
+# jupyter notebook OR python3.9 -m jupyter notebook
+# navigate to directory and create a jupyter python3 notebook
+# import pandas
+# df=pandas.read_csv(path to local csv file)
+# df
