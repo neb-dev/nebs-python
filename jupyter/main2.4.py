416e308e0f5b5b55a06057cbe449123be27aa0e8
neb <10528790@my.uvu.edu>
2021-05-21 22:07:03 -0600

    reading json, xlsx, txt (comma sep), and txt (semi-colon sep) files

--- jupyter/main2.4.py
+++ jupyter/main2.4.py
@@ -7,3 +7,16 @@
 
 # import os
 # os.listdir() - shows files in directory
+# pandas.read_csv(path)
+
+# pandas.read_json(path)
+
+# pip3.9 install xlrd - DEPRICATED for xlsx, will only read xls
+# pip3.9 install openpyxl 
+# pandas.read_xlsx(path, sheetname=0) - read xlsx file
+
+# pandas.read_csv(txt file path)
+
+# pandas.read_csv(txt file path using diff char separator than comma, sep=";")
+
+# pandas.read_csv? - returns read_csv method definition
