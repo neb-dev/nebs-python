e7abd0f77f238e305a82c0991d51fc977328fc67
neb <10528790@my.uvu.edu>
2021-05-23 17:40:52 -0600

    using the geopy library to extract address cordinates with ArcGIS, also lambda

--- main2.6.py
+++ main2.6.py
@@ -3,6 +3,21 @@
 # nom = Nominatim()
 
 # Use this:
+# pip3.9 install geopy
 # from geopy.geocoders import ArcGIS
 # nom = ArcGIS()
 
+# from geopy.geocoders import ArcGIS
+
+# arc = ArcGIS()
+# coords = arc.geocode("3995 23rd St, San Francisco, CA 94114")
+# coords.latitude
+# coords.longitude
+
+# import pandas
+# df = pandas.read_csv("supermarkets.csv")
+# df["Address"] = df["Address"] + ", " + df["City"] + ", " + df["State"] + ", " + df["Country"]
+# df["Coordinates"] = df["Address"].apply(arc.geocode)
+# df["Latitude"] = df["Coordinates"].apply(lambda x: x.latitude if x != None else None)
+# df["Longitude"] = df["Coordinates"].apply(lambda x: x.longitude if x != None else None)
+# df
