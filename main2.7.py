a19349d53d59f5be68577f5f4e07206421434012
neb <10528790@my.uvu.edu>
2021-05-24 21:49:08 -0600

    nusing numpy library

---
+++ main2.7.py
@@ -0,0 +1,13 @@
+# import numpy
+# [[3,21,14,7,15],[],[]] - data type List
+
+# data = numpy.arange(25) - produces single dimension array, similar to python list
+
+# type(data) - numpy.ndarray Data Type
+# data.reshape(5,5) - creates a 5x5 two dimensional array
+
+# data = numpy.arange(27)
+# data = numpy.reshape(27)
+# data.reshape(3,3,3) - creates a 3x3x3 three dimensional array
+
+# data3 = numpy.asarray([[3,21,14,7,15],[],[]]) - convert list to numpy array - data type numpy.ndarray
