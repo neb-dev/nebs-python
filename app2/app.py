85e449b562105a5daca5c14ff06475f5e4e208b9
neb <10528790@my.uvu.edu>
2021-05-28 21:06:55 -0600

    tiles, featuregroup, marker, add children

--- app2/app.py
+++ app2/app.py
@@ -1,6 +1,20 @@
 # pip3.9 install folium
-# import folium
+import folium
 # dir(folium)
-# map = folium.Map(location=[39, -111], zoom=4)
-# map.save("app2/map.html")
-# tiles = "Stamen Terrain"
+
+# create a map object
+map = folium.Map(location=[39, -111], zoom_start="6", tiles="Stamen Terrain")
+
+# FeatureGroup
+fg = folium.FeatureGroup(name="Utah Map")
+
+# add Marker to map
+fg.add_child(folium.Marker(location=[40.3,-111.7], popup="Vineyard", icon=folium.Icon(color="red")))
+
+# add FeatureGroup marker to map object
+map.add_child(fg)
+
+# generate html map
+map.save("app2/map.html")
+
+
