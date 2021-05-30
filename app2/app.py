d793061ec52d4b9d4dedc9b960bf184715a54cad
neb <10528790@my.uvu.edu>
2021-05-29 21:23:47 -0600

    iterate through lists to dynamically add map markers

--- app2/app.py
+++ app2/app.py
@@ -9,7 +9,13 @@ map = folium.Map(location=[39, -111], zoom_start="6", tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="Utah Map")
 
 # add Marker to map
-fg.add_child(folium.Marker(location=[40.3,-111.7], popup="Vineyard", icon=folium.Icon(color="red")))
+# fg.add_child(folium.Marker(location=[40.3,-111.7], popup="Vineyard", icon=folium.Icon(color="red")))
+marker_coords = [[40.3, -111.7], [40.6, -111.4], [37, -113.5]]
+marker_popups = ["Vineyard", "Park City", "St. George"]
+
+# zip returns a zip object that is an iterator of tuples where the value in each tuple is paried together
+for coords, popup in zip(marker_coords, marker_popups):
+    fg.add_child(folium.Marker(location=coords, popup=popup, icon=folium.Icon(color="red")))
 
 # add FeatureGroup marker to map object
 map.add_child(fg)
